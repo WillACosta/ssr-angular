eee49dff508d745f032b43857af9c34e13661ff5
willACosta <williamamaral521@gmail.com>
2022-03-07 07:16:28 -0300

    feat(home): add meta tags to the page and improve SEO

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,19 +1,33 @@
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Title, Meta } from '@angular/platform-browser';
+
 import { Photo } from 'src/app/models';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   photos: Photo[];
 
-  constructor(private readonly _route: ActivatedRoute) {}
+  constructor(
+    private readonly _route: ActivatedRoute,
+    private _title: Title,
+    private _meta: Meta
+  ) {}
 
   ngOnInit() {
+    this.setMetaTags();
     this.photos = this._route.snapshot.data['photos'];
+  }
+
+  private setMetaTags() {
+    this._title.setTitle('SSR Angular Photo Gallery');
+    this._meta.addTags([
+      { name: 'description', content: 'Angular SSR Photo Gallery' },
+      { name: 'author', content: 'Will' },
+      { name: 'keywords', content: 'angular, ssr, unsplash, gallery, photo' }
+    ]);
   }
 }
